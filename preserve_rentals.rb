4d044eae13107815fd70bc54c71510d01c81f7b9
Daf <dafne.azzolina@gmail.com>
2022-08-31 22:25:30 -0300

    Save rentals working

--- preserve_rentals.rb
+++ preserve_rentals.rb
@@ -5,25 +5,25 @@ module PreserveRentals
     # for each book in array, generate a JSON object with it
     # and append it to the JSON file into a new line
     @rentals.each do |rental|
-      puts rental
       array_rental = [rental.person.name, rental.book.title, rental.date]
+      puts array_rental
       json_rental = JSON.generate(array_rental)
       File.write('./rentals.json', "#{json_rental}\n", mode: 'a')
     end
   end
 
   # Retrieve data
-  def retrieve_rentals
-    # if the JSON file is empty, start 'write' mode
-    File.open('./rentals.json', 'w') unless File.exist?('./rentals.json')
-    # if the JSON file is not empty, keep 'append' mode
-    # emptying book array
-    @rentals = []
-    # add books from JSON to book array
-    File.foreach('./rentals.json') do |line|
-      @rentals << JSON.parse(line, create_additions: true)
-    end
-    # return book array
-    @rentals
-  end
+  # def retrieve_rentals
+  #   # if the JSON file is empty, start 'write' mode
+  #   File.open('./rentals.json', 'w') unless File.exist?('./rentals.json')
+  #   # if the JSON file is not empty, keep 'append' mode
+  #   # emptying book array
+  #   @rentals = []
+  #   # add books from JSON to book array
+  #   File.foreach('./rentals.json') do |line|
+  #     @rentals << JSON.parse(line, create_additions: true)
+  #   end
+  #   # return book array
+  #   @rentals
+  # end
 end
