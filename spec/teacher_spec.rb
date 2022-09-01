9e5b71f23d3feb9a223dfbb6e33a18337fcbbe5b
Fabien <fabienswift@gmail.com>
2022-09-01 13:54:52 -0400

    Add more test

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -8,14 +8,17 @@ describe Teacher do
     context 'with valid input' do
       it 'should return a correct age' do
         expect(@new_teacher.age).to eq(35)
+        expect(@new_teacher.age).not_to eq(300)
       end
 
       it 'should return a correct specialization' do
         expect(@new_teacher.specialization).to eq('Programming')
+        expect(@new_teacher.specialization).not_to eq('Maths')
       end
 
       it 'should return a correct name' do
         expect(@new_teacher.name).to eq('Fabien')
+        expect(@new_teacher.name).not_to eq('Fabricio')
       end
     end
   end
